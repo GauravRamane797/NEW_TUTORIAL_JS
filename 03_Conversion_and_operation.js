0a73498bd52f64730b81064d18d400ba92655791
GauravRamane <gauravramane797@gmail.com>
2023-11-21 10:18:02 +0530

    conversion and oprations

---
+++ 03_Conversion_and_operation.js
@@ -0,0 +1,61 @@
+// string values
+ 
+ let score =  "33"
+
+ console.log(typeof score);
+ console.log (typeof(score));
+ 
+ let valueInNumber = Number(score);
+ console.log(valueInNumber);
+ console.log(typeof valueInNumber);
+
+// null values 
+
+ let scores =  null
+
+ console.log(typeof scores);
+ console.log (typeof(scores));
+ 
+ let valueInNumbers = Number(scores);
+ console.log(valueInNumbers);
+ console.log(typeof valueInNumbers);
+
+// undefined values
+
+ let scoress =  undefined
+
+ console.log(typeof scoress);
+ console.log (typeof(scoress));
+ 
+ let valueInNumberss = Number(scoress);
+ console.log(valueInNumberss);
+ console.log(typeof valueInNumberss);
+
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+
+let isLoggedIn = 1
+
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn);
+
+
+// conversions
+
+// 1=> true; 0 =>false
+// '' => false
+// "gaurav" => true
+
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
+
