709128599230a15e53bb438bdce54390b5627db2
GauravRamane <gauravramane797@gmail.com>
2023-11-21 13:30:45 +0530

    Datatypes in javascript

--- 03_Conversion_and_operation.js
+++ 03_Conversion_and_operation.js
@@ -2,34 +2,34 @@
  
  let score =  "33"
 
- console.log(typeof score);
- console.log (typeof(score));
+//  console.log(typeof score);
+//  console.log (typeof(score));
  
  let valueInNumber = Number(score);
- console.log(valueInNumber);
- console.log(typeof valueInNumber);
+//  console.log(valueInNumber);
+//  console.log(typeof valueInNumber);
 
 // null values 
 
  let scores =  null
 
- console.log(typeof scores);
- console.log (typeof(scores));
+//  console.log(typeof scores);
+//  console.log (typeof(scores));
  
  let valueInNumbers = Number(scores);
- console.log(valueInNumbers);
- console.log(typeof valueInNumbers);
+//  console.log(valueInNumbers);
+//  console.log(typeof valueInNumbers);
 
 // undefined values
 
  let scoress =  undefined
 
- console.log(typeof scoress);
- console.log (typeof(scoress));
+//  console.log(typeof scoress);
+//  console.log (typeof(scoress));
  
  let valueInNumberss = Number(scoress);
- console.log(valueInNumberss);
- console.log(typeof valueInNumberss);
+//  console.log(valueInNumberss);
+//  console.log(typeof valueInNumberss);
 
 
 
@@ -42,7 +42,7 @@
 let isLoggedIn = 1
 
 let boolenIsLoggedIn = Boolean(isLoggedIn)
-console.log(boolenIsLoggedIn);
+// console.log(boolenIsLoggedIn);
 
 
 // conversions
@@ -54,8 +54,55 @@ console.log(boolenIsLoggedIn);
 
 let someNumber = 33
 
-let stringNumber = string(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+
+//***************Opearations*****************//
+
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+
+// console.log(2+2);      addition
+// console.log(2-2);        substraction
+// console.log(2*2);        mulitplication
+// console.log(2/2);        module
+// console.log(2**2);       square
+// console.log(2%2);        percentage
+
+
+let str1 = "Gaurav"
+let str2 = " ramane"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log ("1"+ 2);             ---> output 12
+// console.log (1 + "2");            ---> output 12
+// console.log ("1"+ 2 + 2);         ---> output 122
+// console.log (1+ 2 + "2");         ---> output 32
+// console.log (1+ 2);               ---> output 3
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2 , num3
+
+num1 =  num2 =  num3 = 2 + 2
+let gameCounter  = 100
+gameCounter++;            // postfix values
+
+++gameCounter;            // prefix values
+// console.log(gameCounter)
+
+//link to study for prefix and postfix ----->            https://medium.com/hackernoon/javascript-back-to-basics-prefix-vs-postfix-8da5256223d2#:~:text=The%20Postfix%20Form%20returns%20the,value%20after%20the%20increment%2Fdecrement.
+
+
 
 
