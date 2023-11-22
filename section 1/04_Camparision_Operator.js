32ec48ac2f0dde87df18b57426f824e0e0e9c85c
GauravRamane <gauravramane797@gmail.com>
2023-11-22 16:38:22 +0530

    section 1

---
+++ section 1/04_Camparision_Operator.js
@@ -0,0 +1,23 @@
+//      ~~~~~~~~~Camparision Operator~~~~~~~~
+
+// console.log(2 > 1);     greater than
+// console.log(2 >= 1);    greater than is equal to 
+// console.log(2 < 1);     less than
+// console.log(2 <= 1);    less than is equal to
+// console.log(2 == 1);    is equal to
+// console.log(2 != 1);    not equal to
+
+
+// console.log("2" > 1);       true
+// console.log("02" > 1);      true
+
+
+// console.log(null > 0);      false
+// console.log(null == 0);     false
+// console.log(null >= 0);     true
+
+
+//  === 'triple is equal to' "Strict" not only check the values , first check the value and second check the this datatypes by strictly
+
+// == "double is equal to" only check  the values 
+
---
+++ section 1/08_Dates_in.js
@@ -0,0 +1,44 @@
+// # Dates
+
+let myDate = new Date ()
+// console.log(myDate.toString());                    // output--> Wed Nov 22 2023 10:27:16 GMT+0530 (India Standard Time)
+// console.log(myDate.toDateString());                // ouptut--> Wed Nov 22 2023
+// console.log(myDate.toLocaleString());              // output--> 22/11/2023, 10:28:57 am
+// console.log(myDate.toLocaleTimeString());           // ouput--> 10:30:14 am
+
+
+let myCreatedDate = new Date (2023, 0, 23)
+// console.log(myCreatedDate.toDateString())               // output --> Mon Jan 23 2023
+
+let myCreatedDate1 = new Date (2023, 0, 23, 5, 3)
+// console.log(myCreatedDate1.toLocaleString())              // output --> 23/1/2023, 5:03:00 am
+
+
+let myCreatedDate2 = new Date (2023, 0, 23, 5, 3)
+// console.log(myCreatedDate2.toLocaleTimeString())      // output ---> 5:03:00 am
+
+let myCreatedDate3 = new Date (2023, 0, 23, 5, 3)
+// console.log(myCreatedDate3.toISOString())            // 2023-01-22T23:33:00.000Z
+
+
+let myCreateDate =  new Date ("01-14-2023")
+// console.log(myCreateDate.toLocalString());
+
+let myTimeStamp = Date.now ()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+// # you can only find the date,week,month,year
+
+// let newDate = new Date()
+// console.log(newDate)                               // output --->   2023-11-22T05:25:32.923Z
+// console.log(newDate.getTime())                     // output --->   1700630732923
+// console.log(newDate.getMonth())                    // output --->   10
+// console.log(newDate.getUTCFullYear())              // output --->   2023
+// console.log(newDate.getMilliseconds())             // output --->   923
+// console.log(newDate.getSeconds())                  // output --->   32
+// console.log(newDate.getMinutes())                  // output --->   55
+// console.log(newDate.getHours())                    // output --->   10
+// console.log(newDate.getUTCDate())                  // output --->   22  
